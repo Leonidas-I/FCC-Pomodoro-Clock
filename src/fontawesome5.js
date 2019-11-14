49ae4abb1562d747123ac8489699200050443cf4
Leonidas-I <doombringer@disroot.org>
2019-11-14 10:55:44 +0700

    make font awesome 5 into separate file js

---
+++ src/fontawesome5.js
@@ -0,0 +1,3 @@
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faAngleDown, faAngleUp, faPlay, faPause, faSyncAlt} from '@fortawesome/free-solid-svg-icons';
+library.add(faAngleDown, faAngleUp, faPlay, faPause, faSyncAlt);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
 import {Clock} from './clock';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {faAngleDown, faAngleUp, faPlay, faPause, faSyncAlt} from '@fortawesome/free-solid-svg-icons';
-library.add(faAngleDown, faAngleUp, faPlay, faPause, faSyncAlt);
-
+import './fontawesome5';
 
 ReactDOM.render(<Clock />, document.getElementById('app'));
